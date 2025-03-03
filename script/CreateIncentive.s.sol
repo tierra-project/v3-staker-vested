900b7660549cbf2bb37b008c25b7c3022325e820
Darren Bullock <darren.bullock@gppos.net>
2025-03-03 22:25:35 +0200

    mainnet deployments wip

---
+++ script/CreateIncentive.s.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {IUniswapV3Staker} from "../src/interfaces/IUniswapV3Staker.sol";
+import {IERC20Minimal} from "@uniswap/v3-core/contracts/interfaces/IERC20Minimal.sol";
+import {IUniswapV3Pool} from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import {PoolAddress} from "@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol";
+import {MockToken} from "../src/MockToken.sol";
+import {IncentiveId} from "../src/libraries/IncentiveId.sol";
+import {console} from "forge-std/console.sol";
+contract CreateIncentive is Script {
+    function run() public {
+        vm.createSelectFork(vm.rpcUrl("https://bartio.drpc.org"));
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        IUniswapV3Staker staker = IUniswapV3Staker(
+            0xeB877616063CBc2DF98dF8EBc984FBa5AAE7D60f
+        );
+
+        MockToken mockToken = new MockToken();
+
+        mockToken.approve(
+            address(0xeB877616063CBc2DF98dF8EBc984FBa5AAE7D60f),
+            1000000000000000000000000
+        );
+
+        IUniswapV3Staker.IncentiveKey memory key = IUniswapV3Staker
+            .IncentiveKey({
+                rewardToken: IERC20Minimal(address(mockToken)),
+                pool: IUniswapV3Pool(
+                    0x2619c1f283934F997023082b89b57B7F633C3169
+                ),
+                startTime: block.timestamp + 100,
+                endTime: block.timestamp + 10 days,
+                vestingPeriod: 43200,
+                refundee: address(0xe62A4B251d4e8fac52B08Cc2b88F091548426e4C)
+            });
+
+        bytes32 incentiveId = IncentiveId.compute(key);
+
+        staker.createIncentive(key, 10000000000000000000000);
+
+        // Convert bytes32 to hex string
+        string memory incentiveIdHex = bytes32ToHexString(incentiveId);
+
+        // Write the incentiveId as a hex string to a file
+        console.log(incentiveIdHex);
+
+        vm.stopBroadcast();
+    }
+
+    // Helper function to convert bytes32 to a hex string
+    function bytes32ToHexString(
+        bytes32 data
+    ) internal pure returns (string memory) {
+        bytes memory alphabet = "0123456789abcdef";
+        bytes memory str = new bytes(64);
+        for (uint256 i = 0; i < 32; i++) {
+            str[i * 2] = alphabet[uint8(data[i] >> 4)];
+            str[1 + i * 2] = alphabet[uint8(data[i] & 0x0f)];
+        }
+        return string(str);
+    }
+}
---
+++ script/StakeViaTransferFrom.s.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import {Script} from '../lib/forge-std/src/Script.sol';
+import {IUniswapV3Staker} from '../src/interfaces/IUniswapV3Staker.sol';
+import {IERC20Minimal} from '@uniswap/v3-core/contracts/interfaces/IERC20Minimal.sol';
+import {IUniswapV3Pool} from '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import {PoolAddress} from '@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol';
+import {INonfungiblePositionManager} from '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
+import {IUniswapV3Factory} from '@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol';
+import {console2} from 'forge-std/console2.sol';
+
+contract StakeToken is Script {
+    function run() public {
+        vm.createSelectFork(vm.rpcUrl('https://bartio.drpc.org'));
+        vm.startBroadcast(vm.envUint('PRIVATE_KEY'));
+
+        address token0;
+        address token1;
+        uint24 fee;
+        int24 tickLower;
+        int24 tickUpper;
+        uint128 liquidity;
+
+        IUniswapV3Staker staker = IUniswapV3Staker(0xeB877616063CBc2DF98dF8EBc984FBa5AAE7D60f);
+
+        INonfungiblePositionManager nonfungiblePositionManager = INonfungiblePositionManager(
+            0xC0568C6E9D5404124c8AA9EfD955F3f14C8e64A6
+        );
+
+        (, , token0, token1, fee, tickLower, tickUpper, liquidity, , , , ) = nonfungiblePositionManager.positions(
+            34886
+        );
+
+        bytes memory stakeData = abi.encode(
+            IUniswapV3Staker.IncentiveKey({
+                rewardToken: IERC20Minimal(0xeAfb63cc570FEbb131BaC0808Acdd9B9e69B9945),
+                pool: IUniswapV3Pool(0x2619c1f283934F997023082b89b57B7F633C3169),
+                startTime: 1737798405,
+                endTime: 1738662395,
+                vestingPeriod: 43200,
+                refundee: address(0xe62A4B251d4e8fac52B08Cc2b88F091548426e4C)
+            })
+        );
+
+        console.log('this address', msg.sender);
+
+        nonfungiblePositionManager.safeTransferFrom(address(msg.sender), address(staker), 34979, stakeData);
+
+        vm.stopBroadcast();
+    }
+}
