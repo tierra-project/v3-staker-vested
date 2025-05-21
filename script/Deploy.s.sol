8f0cdb60a01a37a77dd5d7b9a58cc04b7782c08f
CLAM <>
2025-05-21 10:35:41 +0200

    deployments and script updates

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,22 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.7.6;
 
-import {Script} from "forge-std/Script.sol";
-import {UniswapV3Staker} from "../src/UniswapV3Staker.sol";
-import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import {Script} from 'forge-std/Script.sol';
+import {UniswapV3Staker} from '../src/UniswapV3Staker.sol';
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.createSelectFork(vm.envString("ALCHEMY_RPC_URL"));
-        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        vm.createSelectFork(vm.envString('ALCHEMY_RPC_URL'));
+        vm.startBroadcast();
 
-        address factoryAddress = vm.envAddress("V3_FACTORY");
-        address nonfungiblePositionManagerAddress = vm.envAddress(
-            "V3_POSITION_MANAGER"
-        );
+        address factoryAddress = vm.envAddress('V3_FACTORY');
+        address nonfungiblePositionManagerAddress = vm.envAddress('V3_POSITION_MANAGER');
 
         new UniswapV3Staker(
             IUniswapV3Factory(factoryAddress),
--- src/MockToken.sol
+++ src/MockToken.sol
@@ -5,7 +5,7 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract MockToken is ERC20, Ownable {
-    constructor() ERC20('MockToken', 'MTK') {
+    constructor() ERC20('FARMREWARDTOKEN', 'FRT') {
         // Mint initial supply to the contract deployer
         _mint(msg.sender, 1000000 * 10 ** 18);
     }
